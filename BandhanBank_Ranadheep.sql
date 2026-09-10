45c54722aa5f1373dbcde54d53bf7691c48e8fd1
Ranadheep Boda <ranadheep17@gmail.com>
2026-09-10 08:35:20 +0530

    Add files via upload

---
+++ BandhanBank_Ranadheep.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{0ED4F300-C6C0-4D7B-890F-74A9C0273789}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {8C02643A-FB7B-4DBB-8451-69F0732EB990}
+	EndGlobalSection
+EndGlobal
